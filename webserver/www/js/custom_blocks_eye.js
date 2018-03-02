0e350a82f76b2b7df837b428af50f0093e9fd801
Sascha Müller zum Hagen <Sascha@muellerzumhagen.de>
2018-03-02 17:27:59 +0100

    Improvement: Eye
    Blockly definition to set the color of an eye.

--- webserver/www/js/custom_blocks_eye.js
+++ webserver/www/js/custom_blocks_eye.js
@@ -21,3 +21,74 @@ Blockly.Python['eye_object'] = function(block) {
     var code = 'Eye(' + argument0 + ')';
     return [code, Blockly.Python.ORDER_ATOMIC];
 };
+
+Blockly.Blocks['eye_colour'] = {
+    init: function() {
+        this.setColour(COLOR_EYE);
+
+        this.appendDummyInput()
+            .appendField('Set')
+            .appendField(new Blockly.FieldVariable('eye'), 'VAR');
+        this.appendDummyInput()
+            .appendField('eye color')
+            .appendField(new Blockly.FieldColour('#ff0000') , 'Colour');
+        this.setInputsInline(true);
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+
+        var thisBlock = this;
+        this.setTooltip(function() {
+            return 'Define for an element this color ' + thisBlock.getFieldValue('Colour');
+        });
+    }
+}
+Blockly.Python['eye_colour'] = function(block) {
+    var varName = Blockly.Python.variableDB_.getName(block.getFieldValue('VAR'), Blockly.Variables.NAME_TYPE);
+    var colour = block.getFieldValue('Colour');
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(colour);
+    var r = parseInt(result[1], 16)
+    var g = parseInt(result[2], 16)
+    var b = parseInt(result[3], 16)
+    var code = varName + '.set_color(' + r + ', ' + g + ', ' + b + ')\n';
+    return code;
+};
+
+Blockly.Blocks['eye_colour_rgb'] = {
+    init: function() {
+        this.setColour(COLOR_EYE);
+
+        this.appendDummyInput()
+            .appendField('Set')
+            .appendField(new Blockly.FieldVariable('eye'), 'VAR');
+        this.appendDummyInput()
+            .appendField('eye color');
+        this.appendValueInput('R')
+            .setCheck('Number')
+            .appendField('Red');
+        this.appendValueInput('G')
+            .setCheck('Number')
+            .appendField('Green');
+        this.appendValueInput('B')
+            .setCheck('Number')
+            .appendField('Blue');
+        this.setInputsInline(true);
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+
+        var thisBlock = this;
+        this.setTooltip(function() {
+            var r = thisBlock.getFieldValue('R');
+            var g = thisBlock.getFieldValue('G');
+            var b = thisBlock.getFieldValue('B');
+            return 'Define for an element this color (' + r + ', ' + g + ', ' + b + ')';
+        });
+    }
+}
+Blockly.Python['eye_colour_rgb'] = function(block) {
+    var varName = Blockly.Python.variableDB_.getName(block.getFieldValue('VAR'), Blockly.Variables.NAME_TYPE);
+    var r = Blockly.Python.valueToCode(block, 'R', Blockly.Python.ORDER_ATOMIC) || '0';
+    var g = Blockly.Python.valueToCode(block, 'G', Blockly.Python.ORDER_ATOMIC) || '0';
+    var b = Blockly.Python.valueToCode(block, 'B', Blockly.Python.ORDER_ATOMIC) || '0';
+    var code = varName + '.set_color(' + r + ', ' + g + ', ' + b + ')\n';
+    return code;
+};
