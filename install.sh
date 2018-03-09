0546d953ab2e00ea428b9d4fa12a433950bb2547
Sascha Müller zum Hagen <Sascha@muellerzumhagen.de>
2018-03-09 15:28:26 +0100

    Remove all symbolic links before create new ones.

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ opkg install curl python python-light pyOnionI2C pyPwmExp libonioni2c
 opkg upgrade libonioni2c pyOnionI2C
 
 # add the hackerschool to the site packages
+rm /usr/lib/python2.7/site-packages/hackerschool
 ln -s "$SCRIPT_DIR/python_lib" /usr/lib/python2.7/site-packages/hackerschool
 
 # add the server to start at boot
