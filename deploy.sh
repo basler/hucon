2ed3c65e7224c6bfc7c73489a6a3e116a323c893
Sascha Müller zum Hagen <Sascha@muellerzumhagen.de>
2018-03-16 19:18:31 +0100

    BugFix: Remove unused files
    The __version__ file is not needed and can be removed after the deployable file is created.

--- deploy.sh
+++ deploy.sh
@@ -27,5 +27,5 @@ sh \$path/install.sh
 exit 0
 __PAYLOAD_BELOW__\n" > "$tmp"
 
-cat "$tmp" "hackerschool.tar.gz" > "hackerschool.run" && rm "$tmp" && rm "hackerschool.tar.gz"
+cat "$tmp" "hackerschool.tar.gz" > "hackerschool.run" && rm "$tmp" && rm "hackerschool.tar.gz" && rm __version__
 chmod +x "hackerschool.run"
