dbc6ed16b258fc08c415ad4b6a2d004e7bceb4da
Jonas Trampe <jonas.trampe@baslerweb.com>
2019-07-01 16:10:19 +0200

    [Critical] Remove unneeded install commands, as this is mostly done by the image installation This WILL break non-image installations and should be improved maybe with an parameter

--- install.sh
+++ install.sh
@@ -7,33 +7,14 @@
 
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-# update the system
-opkg update
-
-# install needed packages
-opkg install curl python-light python-pip pyOnionI2C
-pip install flask
-
 # add the hackerschool to the site packages (only for old code)
-if [ -L /usr/lib/python2.7/site-packages/hackerschool ]; then
-    rm /usr/lib/python2.7/site-packages/hackerschool
+if [ -L $1/usr/lib/python2.7/site-packages/hackerschool ]; then
+	rm $1/usr/lib/python2.7/site-packages/hackerschool
 fi
-ln -s "$SCRIPT_DIR/python_lib" /usr/lib/python2.7/site-packages/hackerschool
+ln -s "/opt/hucon/python_lib" $1/usr/lib/python2.7/site-packages/hackerschool
 
 # add the hucon to the site packages
-if [ -L /usr/lib/python2.7/site-packages/hucon ]; then
-    rm /usr/lib/python2.7/site-packages/hucon
+if [ -L $1/usr/lib/python2.7/site-packages/hucon ]; then
+	rm $1/usr/lib/python2.7/site-packages/hucon
 fi
-ln -s "$SCRIPT_DIR/python_lib" /usr/lib/python2.7/site-packages/hucon
-
-# add the init script as symbolic link
-chmod +x "$SCRIPT_DIR/init.d/i2c_led"
-chmod +x "$SCRIPT_DIR/i2c_led.sh"
-if [ ! -L /etc/init.d/i2c_led ]; then
-    ln -s "$SCRIPT_DIR/init.d/i2c_led" /etc/init.d/i2c_led
-fi
-/etc/init.d/i2c_led enable
-
-# add the server to start at boot
-sed -i '/start_server.sh/d' /etc/rc.local
-sed -i "/^exit 0/i sh $SCRIPT_DIR/start_server.sh >> /tmp/hucon.log 2>&1 & " /etc/rc.local
+ln -s "/opt/hucon/python_lib" $1/usr/lib/python2.7/site-packages/hucon
