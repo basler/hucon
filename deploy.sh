917e3b0fb53c36f6f6627b61e89ccea1931a8238
Jonas Trampe <jonas.trampe@baslerweb.com>
2019-07-02 16:39:09 +0200

    Adopt tarfile creation to image-building

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,15 @@
 # Author: Sascha.MuellerzumHagen@baslerweb.com
 
 BASEDIR=`dirname "${0}"`
+TMP_FILENAME=hucon-${1}.tar.gz
+FILENAME=hucon-${1}.run
+
 cd "$BASEDIR"
+
 
 # compress the needed files into one tar image
 echo $1 > __version__
-tar -czvf hucon.tar.gz __version__ README.md code/ init.d/ python_lib/ webserver/ i2c_led.sh install.sh start_server.sh uninstall.sh update.sh
+tar -czvf "$TMP_FILENAME" __version__ README.md code/ init.d/ python_lib/ webserver/ i2c_led.sh install.sh start_server.sh uninstall.sh update.sh
 
 # generate a self extracting tar image
 tmp=__extract__$RANDOM
@@ -18,20 +22,15 @@ tmp=__extract__$RANDOM
 printf "#!/bin/bash
 PAYLOAD_LINE=\`awk '/^__PAYLOAD_BELOW__/ {print NR + 1; exit 0; }' \$0\`
 
-# Install tar to decompress the image.
-opkg install tar
-
-path=/root/hucon
+path=/opt/hucon
 if [ \$1 ]; then
     path=\"\$1\"hucon
 fi
 
 mkdir -p \$path | tail -n+\$PAYLOAD_LINE \$0 | tar -xvzC \$path
 
-sh \$path/install.sh
-
 exit 0
 __PAYLOAD_BELOW__\n" > "$tmp"
 
-cat "$tmp" "hucon.tar.gz" > "hucon.run" && rm "$tmp" && rm "hucon.tar.gz" && rm __version__
-chmod +x "hucon.run"
+cat "$tmp" "$TMP_FILENAME" > "$FILENAME" && rm "$tmp" && rm "$TMP_FILENAME" && rm __version__
+chmod +x "$FILENAME"
