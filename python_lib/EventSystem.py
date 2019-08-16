56656b6b9abb98b88c2b3005cf364519dbed7536
Sascha Müller zum Hagen <sascha.muellerzumhagen@baslerweb.com>
2019-08-16 14:47:34 +0200

    Set a position of an Event is now possible

--- python_lib/EventSystem.py
+++ python_lib/EventSystem.py
@@ -14,17 +14,29 @@ import json
 GLOB_EVENT_CALLBACK_DICT = None
 
 
-class Button(object):
-    """ Button event object. Can be assigned to one event per instance.
+class EventBase(object):
+    """ Base class for all event based objects.
     """
     callback = None
     x = 0
     y = 0
+    width = 1
+    height = 1
 
-    def __init__(self, register_callback, x=1, y=1):
+    def __init__(self, register_callback, x, y, width, height):
         self.callback = register_callback
         self.x = x
         self.y = y
+        self.width = width
+        self.height = height
+
+
+class ButtonEvent(EventBase):
+    """ Button event object. Can be assigned to one event per instance.
+    """
+
+    def __init__(self, register_callback, x=0, y=0, width=1, height=1):
+        super(Button, self).__init__(register_callback, x, y, width, height)
 
 
 class EventSystem(object):
@@ -40,19 +52,49 @@ class EventSystem(object):
         global GLOB_EVENT_CALLBACK_DICT
 
         GLOB_EVENT_CALLBACK_DICT = {}
+        GRID_WIDTH = 8
+        GRID_HEIGHT = 8
 
         # Generate a map of numbers to handle the events and add it to the events to catch
         index = 0
         events_to_catch = {}
+
+        for y in range(GRID_HEIGHT):
+            for x in range(GRID_WIDTH):
+                if 'Y' not in events_to_catch:
+                    events_to_catch['Y'] = {}
+
+                if str(y) not in events_to_catch['Y']:
+                    events_to_catch['Y'][str(y)] = {}
+
+                if 'X' not in events_to_catch['Y'][str(y)]:
+                    events_to_catch['Y'][str(y)]['X'] = {}
+
+                if str(x) in events_to_catch['Y'][str(y)]['X']:
+                    print('[red] Error')
+
+                events_to_catch['Y'][str(y)]['X'][str(x)] = None
+
         for key in events_dict:
             value = events_dict[key]
 
             # Add a simple button event
             GLOB_EVENT_CALLBACK_DICT[index] = value.callback
-            if 'Button' in events_to_catch:
-                events_to_catch['Button'] += [{'Name': key, 'Event': index, 'X': value.x, 'Y': value.y}]
-            else:
-                events_to_catch['Button'] = [{'Name': key, 'Event': index, 'X': value.x, 'Y': value.y}]
+
+            # check if the space is empty to put the object into it
+            for y in range(value.y, value.y + value.height):
+                for x in range(value.x, value.x + value.width):
+                    if events_to_catch['Y'][str(y)]['X'][str(x)] is not None:
+                        name = events_to_catch['Y'][str(y)]['X'][str(x)]['Name']
+                        raise Exception("[red] Error: '%s' and '%s' are using the same space (x: %s / y: %s)" % (key, name, x, y))
+
+            events_to_catch['Y'][str(value.y)]['X'][str(value.x)] = {'Type': 'Button', 'Name': key, 'Event': index, 'Width': value.width, 'Height': value.height}
+
+            # Use a place holder to signalize a none empty space
+            for y in range(value.y, value.y + value.height):
+                for x in range(value.x, value.x + value.width):
+                    if events_to_catch['Y'][str(y)]['X'][str(x)] is None:
+                        events_to_catch['Y'][str(y)]['X'][str(x)] = {'Type': 'PlaceHolder', 'Name': key}
 
             # Increment the index
             index += 1
