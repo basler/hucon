b139c4ede4606aef47e3d1df248669e12700009c
Sascha Müller zum Hagen <sascha.muellerzumhagen@baslerweb.com>
2020-01-29 15:44:39 +0100

    Documentation build prepared for multi language support

---
+++ generate_mkdocs_yml.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" generate_mkdocs_yml.py - Generate the mkdocs config files for the different
+                             languages based on the config template and doc config json file.
+
+    Copyright (C) 2020 Basler AG
+    All rights reserved.
+
+    This software may be modified and distributed under the terms
+    of the BSD license.  See the LICENSE file for details.
+"""
+
+from jinja2 import Template
+import json
+
+with open('docs_config.json') as json_file:
+    config = json.load(json_file)
+
+    for language in config['language']:
+
+        with open('mkdocs-template.yml') as tmp_file:
+            tmp = Template(tmp_file.read())
+            msg = tmp.render(language=language, description=config['language'][language]['description'])
+
+            with open('mkdocs-%s.yml' % language, 'w') as lang_file:
+                lang_file.write(msg)
