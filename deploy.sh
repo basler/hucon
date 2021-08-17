f347104ba912a998c58dff9c41de9350733f7fa0
Jonas Trampe <jonas@jonas-trampe.de>
2021-08-17 16:59:47 +0200

    Enable js and css packing

--- deploy.sh
+++ deploy.sh
@@ -11,13 +11,40 @@
 BASEDIR=`dirname "${0}"`
 TMP_FILENAME=hucon-${1}.tar.gz
 FILENAME=hucon-${1}.run
-
+RELEASE_DIR=release
+TEMP_DIR=$RELEASE_DIR/temp
 cd "$BASEDIR"
 
+# create release dir, create version file and copy to the release folder
+mkdir -p $TEMP_DIR
+echo $1 > __version__
+cp LICENSE README.md code/ init.d/ python_lib/ webserver/ i2c_led.sh install.sh img_install.sh start_server.sh uninstall.sh update.sh $RELEASE_DIR/tmp
+
+cd $TEMP_DIR
+#minification of JS files
+find js/ -type f \
+    -name "*.js" ! -name "*.min.*" ! -name "vfs_fonts*" \
+    -exec echo {} \; \
+    -exec yui-compressor -o {}.min {} \; \
+    -exec rm {} \; \
+    -exec mv {}.min {} \;
+
+#minification of CSS files
+find css/ -type f \
+    -name "*.css" ! -name "*.min.*" \
+    -exec echo {} \; \
+    -exec yui-compressor -o {}.min {} \; \
+    -exec rm {} \; \
+    -exec mv {}.min {} \;
 
 # compress the needed files into one tar image
-echo $1 > __version__
 tar -czvf "$TMP_FILENAME" __version__ LICENSE README.md code/ init.d/ python_lib/ webserver/ i2c_led.sh install.sh img_install.sh start_server.sh uninstall.sh update.sh
+
+mv "$TMP_FILENAME" ..
+
+cd ..
+
+rm -rf $TEMP_DIR
 
 # generate a self extracting tar image
 tmp=__extract__$RANDOM
